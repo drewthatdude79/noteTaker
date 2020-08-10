b98da21ef82695fcedeccd03ab8cf93a2db8916e
Andrew Bolton <drupy19@yahoo.com>
2020-08-10 11:01:13 -0400

    adding more stuff

--- API routes/htmlRoutes.js
+++ API routes/htmlRoutes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const path = require("path");
 
-    router.get("/notes", function(req, res) {
+module.exports = function(app) {
+
+    app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../notes.html"));
     });
 
-    router.get ("*", function(req, res) {
+    app.get ("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../index.html"));
     });
-
-    module.exports = router;
+};
